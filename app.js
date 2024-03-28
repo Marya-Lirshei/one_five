5bdb0c1330878c283d0b15fde8d21093a65f3b32
marya <you@example.com>
2024-03-28 14:49:02 +0300

    переподключила свайпер, добавила комьютерную версию

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
-  
-    // // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
-  });
+new Swiper('.swiper',{
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  slidesPerView: 'auto',
+  spaceBetween:16,
+
+});
+
+let text = document.querySelector('.show-all');
+let one = document.querySelector('.second-half__wrapper');
+let two = document.querySelector('.desctop-content');
+let show = document.querySelector('.show-all__text');
+let arrow = document.querySelector('.show-all img');
+
+  text.addEventListener('click',function(){
+    one.classList.toggle('second-half__wrapper--click');
+    two.classList.toggle('desctop-content--click');
+    arrow.classList.toggle('show-all__arrow');
+
+    let txt = show.textContent;
+    show.textContent = (txt === 'Показать все' && 'Скрыть все') || 'Показать все';
+
+
+  })
+
+
+
